95cb534479c0ad199265398482bc446120bd5ce8
Mark Lohstroh <mlohstroh@gmail.com>
2018-11-10 00:16:41 -0600

    just sam testing things out

--- src/Commands.cs
+++ src/Commands.cs
@@ -13,6 +13,7 @@ namespace Jabber
         public static void Register()
         {
             CommandDispatcher.Instance.RegisterCommand("!hello", HelloWorld);
+            CommandDispatcher.Instance.RegisterCommand("!instructions", GetInstructions);
         }
 
         public static async Task HelloWorld(Message msg)
@@ -23,6 +24,21 @@ namespace Jabber
                 await JabberClient.Instance.SendGroupMessage(who.Bare, "Hello Cruel World!");
             else
                 await JabberClient.Instance.SendMessage(who.Bare, "Hello Cruel World!");
+        }
+
+        public static async Task GetInstructions(Message msg)
+        {
+            var who = msg.From;
+            Instruction tmpInstruction = new Instruction("This is our instructions", "samuel_the_terrible");
+
+            if (msg.Type == MessageType.GroupChat)
+            {
+                await JabberClient.Instance.SendGroupMessage(who.Bare, tmpInstruction.ToString());
+            } 
+            else
+            {
+                await JabberClient.Instance.SendMessage(who.Bare, tmpInstruction.ToString());
+            }
         }
     }
 }
---
+++ src/Instruction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jabber
+{
+    public class Instruction
+    {
+        private string instructions;
+        private string setAt;
+        private string setBy;
+
+        public Instruction(string instructions, string setBy)
+        {
+            //TODO Is the person allowed to set instructions?
+            if(false)
+            {
+                //Reject with error message
+            }
+
+            this.Instructions = instructions;
+            this.SetBy = setBy;
+            this.SetAt = DateTime.Now.ToString();
+
+        }
+
+        public string Instructions { get => instructions; set => instructions = value; }
+        public string SetAt { get => setAt; set => setAt = value; }
+        public string SetBy { get => setBy; set => setBy = value; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}\n Set By: {1} @ {2}", Instructions, SetBy, SetAt);
+        }
+    }
+}
